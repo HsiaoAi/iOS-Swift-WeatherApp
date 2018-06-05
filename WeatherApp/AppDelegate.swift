636e366620b0cf4a5607b97f5fc3c74bb1268c00
hsiaoai <ilct227@gmail.com>
2018-06-05 23:54:17 +0800

    Add icon and font

--- WeatherApp/AppDelegate.swift
+++ WeatherApp/AppDelegate.swift
@@ -15,7 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        initRootViewController()
+        
         return true
     }
 
@@ -41,6 +43,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
+}
 
+extension AppDelegate {
+    func initRootViewController() {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        let rootViewController = UIViewController()
+        window.rootViewController = rootViewController
+        window.makeKeyAndVisible()
+        self.window = window
+    }
 }
 
